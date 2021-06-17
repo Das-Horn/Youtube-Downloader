08b5dbec8262ffc5288b3f5f7b3b788f4229f839
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2021-06-17 16:04:55 +0100

    Some frontend scripting and css

---
+++ V2/www/index.js
@@ -0,0 +1,20 @@
+var mp3State = false;
+var currentTitle = "Random Song";
+var currentThumbnailURL = "";
+
+function mp3toggle(){
+    const tgl = document.getElementById("MP3-button");
+    if(mp3State){
+        mp3State = false;
+        tgl.style.backgroundColor = "var(--deactive-color)";
+    }else {
+        mp3State = true;
+        tgl.style.backgroundColor = "var(--active-color)";
+    }
+}
+
+function download(){
+    const queueBox = '<div class="queue-box"><p class="Text queue-text">'+currentTitle+'</p><p class="Text queue-Status">Downloading</p><div class="progress-cont"><div class="progress-bar"><p class="Text progress-text">0%</p></div></div></div>';
+    const container = document.getElementById("queue-cont");
+    container.innerHTML += queueBox;
+}
